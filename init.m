6845dbde00e659e7be86aa24719333e27c654284
Jinwoo Kim <kidjinwoo@me.com>
2023-07-20 18:12:37 +0900

    no message

--- init.m
+++ init.m
@@ -4,12 +4,12 @@
 __attribute__((constructor)) static void init() {
     setenv("MTL_HUD_ENABLED", "1", 1);
     
-    char *path;
+    const char *path;
     if (kCFCoreFoundationVersionNumber >= 2042.1020) {
         path = "/Symbols/usr/lib/libMTLHud.dylib";
     } else {
         path = "/usr/lib/libMTLHud.dylib";
     }
-    
+
     dlopen(path, RTLD_NOW);
 }
